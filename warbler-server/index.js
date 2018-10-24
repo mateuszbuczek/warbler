de4b11c7cc31d033ef161cb8d9bc494e99331043
Mateusz Buczek <this.mateuszbuczek@gmail.com>
2018-10-24 11:19:48 +0200

    with messages and user ver

--- warbler-server/index.js
+++ warbler-server/index.js
@@ -5,13 +5,37 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const errorHandler = require("./handlers/error");
 const authRoutes = require("./routes/auth");
+const messagesRoutes = require("./routes/messages");
+const { loginRequired, ensureCorrectUser } = require("./middleware/auth");
+const db = require("./models");
+
 const PORT = 8081;
 
 app.use(cors());
 app.use(bodyParser.json()); //working with API
 
-// all my routes here - they will come later
+// all my routes here
 app.use("/api/auth", authRoutes);
+app.use(
+  "/api/users/:id/messages",
+  loginRequired,
+  ensureCorrectUser,
+  messagesRoutes
+);
+
+app.get("/api/messages", loginRequired, async function(req, res, next) {
+  try {
+    let messages = await db.Message.find()
+      .sort({ createdAt: "desc" })
+      .populate("user", {
+        username: true,
+        profileImageUrl: true
+      });
+    return res.status(200).json(messages);
+  } catch (e) {
+    return next(e);
+  }
+});
 
 app.use(function(req, res, next) {
   let err = new Error("Not found");
--- warbler-server/models/index.js
+++ warbler-server/models/index.js
@@ -9,3 +9,5 @@ mongoose.connect(
 );
 
 module.exports.User = require("./user");
+module.exports.Message = require("./message");
+
